013e476dc3767dec14f9323965d62eab6972a22c
sanjoyatpipeline <s.biswas@pipelinesecurity.net>
2020-07-17 14:51:06 +0000

    sixthcommit

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ from flask import Flask, render_template, request
 app=Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
+    return render_template('main_page.html')
+
+@app.route('/index',methods=['GET'])
+def index_phish():
     return render_template('index.html')
 
+
+@app.route('/index_phishscore')
+def index_phishscore():
+    return render_template('index_phishscore.html')
 
 
 
